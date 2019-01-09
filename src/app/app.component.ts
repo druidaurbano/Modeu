52a021b3a60ecfe50952f02dc5210dd0ae16c0af
William Pietro <william.carvalho.br@gmail.com>
2019-01-09 17:27:48 -0200

    Atualização

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,8 +23,8 @@ export class MyApp {
 
     // used for an example of ngFor and navigation
     this.pages = [
-      { title: 'Home', component: HomePage },
-      { title: 'List', component: ListPage }
+      { title: 'Menu Inicial', component: HomePage },
+      { title: 'Lista', component: ListPage }
     ];
 
   }
